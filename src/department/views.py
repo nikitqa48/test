f3b5f110fbdd2db948e896e60524d724d5ee8b80
nikitqa48 <Princen1k@yandex.com>
2023-06-29 17:43:33 +0300

    add aggregate func to viewset

--- src/department/views.py
+++ src/department/views.py
@@ -1,8 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import permissions, response, status
 from .serializers import DepartmentSerializer, DepartmentPersonalSerializer
-from django.shortcuts import render, get_object_or_404
-from src.personal.models import CustomUser
+from django.db.models import Sum, Count, Q
 from . import models
 
 
@@ -11,11 +10,11 @@ class DeportmentViewset(ModelViewSet):
     permission_classes = (permissions.AllowAny, )
     queryset = models.Department.objects.all()
 
-    def destroy(self, request, pk, *args, **kwargs):
-        personal = get_object_or_404(CustomUser, id=self.request.query_params.get('personal'))
-        personal.department = None
-        personal.save()
-        return response.Response(status.HTTP_204_NO_CONTENT)
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        data = models.Department.objects.aggregate(salary_sum=Sum('personal__salary'), count_personal=Count('personal'))
+        response.data.append(data)
+        return response
 
 
 class DepartmentPersonalView(ModelViewSet):
@@ -33,6 +32,4 @@ class DepartmentPersonalView(ModelViewSet):
             member.save()
         return response.Response(status=status.HTTP_204_NO_CONTENT)
 
-        # if serializer.is_valid():q
-        #     print('ok')
 
